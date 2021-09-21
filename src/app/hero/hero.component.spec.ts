55d8420e1476a446dbda53fa698d04a00e9bee33
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2021-09-22 00:25:44 +0530

    2nd day session

---
+++ src/app/hero/hero.component.spec.ts
@@ -0,0 +1,16 @@
+import { HeroComponent } from './hero.component'
+
+describe('HeroComponent', () => {
+    let component: HeroComponent;
+
+    beforeEach(() => {
+        component = new HeroComponent()
+    })
+
+    it('should have a correct hero', () => {
+        component.hero = { id: 3, name: 'Super Dude', strength: 8 };
+
+        expect(component.hero.name).toBe('Super Dude')
+    })
+
+})
---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,31 @@
+import { of } from 'rxjs';
+import { HeroesComponent } from './heroes.component'
+
+describe('HeroesComponent', () => {
+    let component: HeroesComponent;
+    let HEROES;
+    let mockHeroService;
+
+    beforeEach(() => {
+        HEROES = [
+            { id: 1, name: 'Spider Dude', strength: 8 },
+            { id: 2, name: 'Wonderful Women', strength: 8 },
+            { id: 3, name: 'Super Dude', strength: 8 }
+        ];
+
+        mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+        component = new HeroesComponent(mockHeroService);
+    })
+
+    describe('delete', () => {
+        it('shoul remove the indecated hero from the heroes list', () => {
+            mockHeroService.deleteHero.and.returnValue(of(true));
+            component.heroes = HEROES;
+
+            component.delete(HEROES[2]);
+
+            expect(component.heroes.length).toBe(2)
+        })
+    })
+
+})
